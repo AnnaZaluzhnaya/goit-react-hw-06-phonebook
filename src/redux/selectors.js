a7076b2d1da0a45ddac38ed5b7227b8421a36ca3
AnnaZaluzhnaya <92920694+AnnaZaluzhnaya@users.noreply.github.com>
2022-08-16 10:48:01 +0200

    update

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,2 @@
 export const getContacts = state => state.contacts.items;
 export const getFilteredContacts = state => state.contacts.filter;
-export const getFilter = () => {
-  return getContacts.filter(contact =>
-    contact.name.toLowerCase().includes(getFilteredContacts)
-  );
-};
